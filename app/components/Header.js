cd3dfe9a82fb49a105fb3bc43a6c59bbaf11d00a
Asbjørn Enge <asbjorn@hanafjedle.net>
2016-05-06 21:47:32 +0200

    Added github link

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import Svg from '@asbjornenge/react-svg'
+import githubMark from '../graphics/github-big-logo.svg'
 
 export default class Header extends React.Component {
     render() {
         return (
             <div className="Header">
               r33d.link
+              <a href="https://github.com/asbjornenge/r33dlink-app" target="_blank">
+                <Svg className="githubMark" svg={githubMark} />
+              </a>
             </div>
         )
     }
